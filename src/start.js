357840513e891fadf70c4acea76060b838b0c51b
mridpin <mridao99@gmail.com>
2021-03-21 10:04:00 +0100

    fix ATTEMPT for prod version

--- src/start.js
+++ src/start.js
@@ -2,6 +2,10 @@ import app from './api/server.js';
 import config from './config/config.js';
 import logger from './utils/logger.js';
 
+logger.info(`CLIENT ID: ${config.CLIENT_ID}`);
+logger.info(`CLIENT SECRET: ${config.CLIENT_SECRET}`);
+logger.info(`AUTH: ${config.spotifyAuthClient}`);
+
 app.listen(config.port, () => {
   logger.info(`Server started at http://localhost:${config.port}`);
 });
